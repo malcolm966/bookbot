8f8ba8b2c0e1b4a33b317818962083672d1bac35
malcolm <malcolm19880513@gmail.com>
2025-12-15 10:18:31 +0900

    a description of your changes here

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import get_number_of_words,count_characters
+from stats import get_number_of_words,count_characters,get_book_text,get_characters_count_sort_by_count
+import sys
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        return f.read()
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    print(f'Usage: python3 main.py <path_to_book>')
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     num_words = get_number_of_words(book_text)
-    print(count_characters(book_text))
+    characters_count_sort_by_count = get_characters_count_sort_by_count(book_text)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {filepath}')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('--------- Character Count -------')
+    for i in characters_count_sort_by_count:
+        print(f'{i['char']}:{i['num']}')
+    print('============= END ===============')
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
 def get_number_of_words(book_text):
     total_words = len(book_text.split())
     return total_words
@@ -11,3 +15,32 @@ def count_characters(book_text):
         else:
             character_dic[c] = 1
     return character_dic
+
+def get_characters_count_sort_by_count(book_text):
+    result_dic = {}
+    book_text = book_text.lower()
+    for c in book_text:
+        if c in result_dic:
+            result_dic[c]['num'] += 1
+        else:
+            if c >= 'a' and c <= 'z':
+                result_dic[c] = {"char":c, "num" : 1}
+    result_list = []
+    for i in result_dic:
+        result_list.append(result_dic[i])
+    result_list.sort(key=sort_on,reverse=True)
+    return result_list
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(items):
+    return items['num']
+
+# vehicles = [
+#     {"name": "car", "num": 7},
+#     {"name": "plane", "num": 10},
+#     {"name": "boat", "num": 2}
+# ]
+# vehicles.sort(reverse=True, key=sort_on)
+# print(vehicles)
+# # [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
